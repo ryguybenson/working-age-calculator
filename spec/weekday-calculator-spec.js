cdf81aa1716c69cf3604d8734e69dad55a6a6112
Riley Benson <ryguybenson@yahoo.com>
2019-03-15 15:09:20 -0700

    create functions and tests for time remaining on all planets

--- spec/weekday-calculator-spec.js
+++ spec/weekday-calculator-spec.js
@@ -7,16 +7,32 @@ describe ('Age', function() {
     let age = new Age(20)
     expect(age.mercuryChecker()).toEqual(4.8);
   })
-  it('should return a normal mercury age', function() {
+  it('should give the expected time remaining on mercury', function() {
+    let age = new Age(20)
+    expect(age.mercuryLifeChecker()).toEqual(75.2)
+  })
+  it('should return a normal venus age', function() {
     let age = new Age(20)
     expect(age.venusChecker()).toEqual(12.4);
   })
-  it('should return a normal mercury age', function() {
+  it('should give the expected time remaining on venus', function() {
+    let age = new Age(20)
+    expect(age.venusLifeChecker()).toEqual(67.6)
+  })
+  it('should return a normal mars age', function() {
     let age = new Age(20)
     expect(age.marsChecker()).toEqual(37.6);
   })
-  it('should return a normal mercury age', function() {
+  it('should give the expected time remaining on mars', function() {
+    let age = new Age(20)
+    expect(age.marsLifeChecker()).toEqual(42.4)
+  })
+  it('should return a normal jupiter age', function() {
     let age = new Age(20)
     expect(age.jupiterChecker()).toEqual(237.2);
+  })
+  it('should give the expected time remaining on jupiter', function() {
+    let age = new Age(20)
+    expect(age.jupiterLifeChecker()).toEqual(-157.2)
   })
 })
--- src/calculator.js
+++ src/calculator.js
@@ -3,13 +3,22 @@ export class Age {
 
   constructor(age){
     this.age = age;
+    this.expected = 80;
   }
-
+//PLANET AGE CALCULATOR FUNCTIONS
   mercuryChecker() {
     let age = this.age;
     const mercury = 0.24;
     let answer = (age * mercury);
     return answer;
+  }
+  mercuryLifeChecker() {
+    let age = this.age;
+    const mercury = 0.24;
+    let answer = (age * mercury);
+    let expected = this.expected;
+    let remaining = (expected - answer);
+    return remaining;
   }
 
   venusChecker() {
@@ -18,18 +27,43 @@ export class Age {
     let answer = (age * venus);
     return answer;
   }
-
+  venusLifeChecker() {
+    let age = this.age;
+    const mercury = 0.62;
+    let answer = (age * mercury);
+    let expected = this.expected;
+    let remaining = (expected - answer);
+    return remaining;
+  }
   marsChecker() {
     let age = this.age;
     const mars = 1.88;
     let answer =  (age * mars);
     return answer;
   }
-
+  marsLifeChecker() {
+    let age = this.age;
+    const mercury = 1.88;
+    let answer = (age * mercury);
+    let expected = this.expected;
+    let remaining = (expected - answer);
+    return remaining;
+  }
   jupiterChecker() {
     let age = this.age;
     const jupiter = 11.86;
     let answer =  (age * jupiter);
     return answer;
   }
+  jupiterLifeChecker() {
+    let age = this.age;
+    const mercury = 11.86;
+    let answer = (age * mercury);
+    let expected = this.expected;
+    let remaining = (expected - answer);
+    return remaining;
+  }
+  //PLANET LIFE EXPECTANCY FUNCTIONS
+
+
 }
